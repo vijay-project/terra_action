5240e405baf5f712a7d1f10ba5b7b862b4da5208
vijaypatelworld@gmail.com <vijaypatelworld@gmail.com>
2021-09-17 18:56:21 +0530

    change1

--- azurerm_redis_linked_server/main.tf
+++ azurerm_redis_linked_server/main.tf
@@ -3,5 +3,5 @@ resource "azurerm_redis_linked_server" "default" {
   linked_redis_cache_location = var.linked_redis_cache_location
   target_redis_cache_name     = var.target_redis_cache_name
   resource_group_name         = var.resource_group_name
-  server_role                 = var.server_roled
+  server_role                 = var.server_role
 }
